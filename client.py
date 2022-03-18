85e4138c639c84886529184b7a644c7b5917a385
Alexander Brand <alexbrand09@gmail.com>
2022-03-17 20:55:23 -0400

    retries

--- client.py
+++ client.py
@@ -13,14 +13,17 @@ helloworld_pb2, helloworld_pb2_grpc = grpc.protos_and_services(
 
 async def sayHello(stub, count):
     try:
-        async for attempt in AsyncRetrying(stop=stop_after_attempt(5), wait=wait_random_exponential(multiplier=0.2, max=1)):
+        start = time.monotonic()
+        print(f"request={count} start={time.monotonic()}")
+        async for attempt in AsyncRetrying(stop=stop_after_attempt(5), wait=wait_random_exponential(multiplier=1, max=10)):
             with attempt:
                 response = await stub.SayHello(helloworld_pb2.HelloRequest(name=f"you #{count}"))
                 print("Greeter client received: " + response.message)
+                print(f"request={count} latency={time.monotonic() - start}")
                 return response
-    except Exception as ex:
-        print(f"{ex}")
-        pass
+    except RetryError as re:
+        print(f"request={count} latency={time.monotonic() - start}")
+        re.reraise()
 
 async def main():
     logging.basicConfig()
@@ -29,8 +32,22 @@ async def main():
     channel = grpc.aio.secure_channel(server_url, ssl_credentials) 
     stub = helloworld_pb2_grpc.GreeterStub(channel)
 
-    tasks = [sayHello(stub, i) for i in range(0, 10000)]
-    await asyncio.gather(*tasks, return_exceptions=True)
+    tasks = [sayHello(stub, i) for i in range(0, 10_000)]
+    res = await asyncio.gather(*tasks, return_exceptions=True)
+
+    summary = {"success":0}
+    for r in res:
+        if not isinstance(r, grpc.aio.AioRpcError):
+            summary["success"] += 1
+            continue
+        
+        d = r.details()
+        if d not in summary:
+            summary[d] = 1
+        else:
+            summary[d] += 1
+
+    print(summary)
 
 if __name__ == '__main__':
     asyncio.run(main())

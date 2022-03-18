b7e37d05dd31a64d94e1c3a4be1cee949a8a50bf
Alexander Brand <alexbrand09@gmail.com>
2022-03-17 21:20:24 -0400

    bring all approaches together

--- client.py
+++ client.py
@@ -11,7 +11,25 @@ import random
 helloworld_pb2, helloworld_pb2_grpc = grpc.protos_and_services(
     "helloworld.proto")
 
+NUM_REQUESTS=10_000
+
 async def sayHello(stub, count):
+    response = await stub.SayHello(helloworld_pb2.HelloRequest(name=f"you #{count}"))
+    print("Greeter client received: " + response.message)
+
+async def gather_with_concurrency(n, *tasks):
+    semaphore = asyncio.Semaphore(n)
+
+    async def sem_task(task):
+        async with semaphore:
+            return await task
+    return await asyncio.gather(*(sem_task(task) for task in tasks), return_exceptions=True)
+
+async def test_semaphores(stub, count):
+    tasks = [sayHello(stub, i) for i in range(NUM_REQUESTS)]
+    return await gather_with_concurrency(count, *tasks)
+
+async def sayHelloWithRetries(stub, count):
     try:
         start = time.monotonic()
         print(f"request={count} start={time.monotonic()}")
@@ -25,6 +43,10 @@ async def sayHello(stub, count):
         print(f"request={count} latency={time.monotonic() - start}")
         re.reraise()
 
+async def test_retries(stub):
+    tasks = [sayHelloWithRetries(stub, i) for i in range(0, NUM_REQUESTS)]
+    return await asyncio.gather(*tasks, return_exceptions=True)
+
 async def main():
     logging.basicConfig()
     server_url = os.getenv("SERVER_URL", "localhost:8081")
@@ -32,9 +54,8 @@ async def main():
     channel = grpc.aio.secure_channel(server_url, ssl_credentials) 
     stub = helloworld_pb2_grpc.GreeterStub(channel)
 
-    tasks = [sayHello(stub, i) for i in range(0, 10_000)]
-    res = await asyncio.gather(*tasks, return_exceptions=True)
-
+    res = await test_semaphores(stub, count=100)
+    #res = await test_retries(stub)
     summary = {"success":0}
     for r in res:
         if not isinstance(r, grpc.aio.AioRpcError):
